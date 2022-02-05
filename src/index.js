9e0f17bab047e8d9d5d2b317ad9319e8729db78a
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-02-05 08:06:42 -0300

    feat: validating CPF exists

--- src/index.js
+++ src/index.js
@@ -12,12 +12,20 @@ app.post("/account", (req, res) => {
 
   const id = uuidv4();
 
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if(customerAlreadyExists) {
+    return res.status(400).json({ message: "Customer Already Exists!"})
+  }
+
   customers.push({
     id,
     cpf,
     name,
     statement: [],
-  })
+  });
 
   return res.status(201).send();
 });
