c340cbe2f3e462e569465f5a6c42d31463e2e639
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-02-06 10:31:52 -0300

    feat: creating account deposit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { request } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
@@ -49,5 +50,22 @@ app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
 
   return response.json(customer.statement);
 })
+
+app.use('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: 'credit'
+  }
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
 
 app.listen(3333);
