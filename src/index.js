1df271f5aefdb40ee47ab7399bce71e69a10390b
Gabriel Martins <martinsgabriel1956@gmail.com>
2022-02-06 09:28:33 -0300

    feat: middlewares

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ app.use(express.json());
 
 const customers = [];
 
+const verifyIfExistsAccountCPF = (request, response, next) => {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if(!customer) {
+    return response.status(400).send({ error: 'Customer not found' });
+  };
+
+  request.customer = customer;
+
+  return next();
+};
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -30,14 +44,8 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get('/statement', (request, response) => {
-  const { cpf } = request.headers;
-
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if(!customer) {
-    return response.status(400).send({ error: 'Customer not found' });
-  };
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer} = request;
 
   return response.json(customer.statement);
 })
